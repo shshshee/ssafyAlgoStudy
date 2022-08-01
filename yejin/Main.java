5b4c66b2f6753c351487cd861c0b5338f8ef5a71
HongYejin <33932851+YeJinHong@users.noreply.github.com>
2022-08-01 22:14:04 +0900

    Update Main.java

--- yejin/Main.java
+++ yejin/Main.java
@@ -1,10 +1,32 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) {
-	
-	
+	static int count = 0;
+	public static int solve(String input) {
+//		System.out.print(input +" => ");
+		
+		int x = Integer.parseInt(input);
+		if(x < 10) {
+			return x;
+		}
+		
+		count++;
+		x = 0;
+		char[] chars = input.toCharArray();
+		for(char c : chars) {
+			x += Character.getNumericValue(c);
+		}
+		
+		return solve(String.valueOf(x));
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		String input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+		
+		int x = solve(input);
+		System.out.println(count);
+		System.out.println(x%3 == 0 ? "YES" : "NO");
 	}
 }
