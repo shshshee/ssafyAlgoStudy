88772b5ce41b912d980c30bc43ab949aa3b5c757
이해민 <2haemin98@gmail.com>
2022-09-29 21:37:22 +0900

    Update 5648

--- 이해민/Solution_5648.java
+++ 이해민/Solution_5648.java
@@ -1,12 +1,9 @@
 package swea.p5648;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -18,15 +15,16 @@ public class Solution_5648 {
 	
 	private static int[][] delta = {{0,1}, {0,-1}, {-1,0}, {1,0}};//상하좌우
 	private static int result;
-	private static List<Pair> lists;
+	private static Queue<Point> queue;
+	private static int[][] maps;//원자 위치
 	
-	static class Pair implements Comparable<Pair>{
+	static class Point {
 		int x;
 		int y;
 		int dir;
 		int energy;
 		
-		public Pair(int x, int y, int dir, int energy) {
+		public Point(int x, int y, int dir, int energy) {
 			super();
 			this.x = x;
 			this.y = y;
@@ -35,98 +33,102 @@ public class Solution_5648 {
 		}
 		
 		@Override
-		public int compareTo(Pair o) {
-			if(this.x == o.x && this.y == o.y) return this.energy - o.energy;
-			return this.x - o.x;
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + ", dir=" + dir + ", energy=" + energy + "]";
 		}
+		
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
 		
-		for (int test_case = 0; test_case <= T; test_case++) {
+		for (int test_case = 1; test_case <= T; test_case++) {
 			int N = Integer.parseInt(br.readLine());//원자의 개수
 			
-			lists = new LinkedList<Pair>();
+			queue = new LinkedList<Point>();
+			
+			maps = new int[4001][4001];
+			result = 0;
 			
 			StringTokenizer st = null;
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				//좌표를 1000 <= x,y <= 2000으로 바꾸어 줌 
+				
 				int x = Integer.parseInt(st.nextToken()) + 1000;
 				int y = Integer.parseInt(st.nextToken()) + 1000;
 				int direction = Integer.parseInt(st.nextToken());
 				int k = Integer.parseInt(st.nextToken());
-				lists.add(new Pair(x, y, direction, k));
+				queue.add(new Point(x*2, y*2, direction, k));
+				maps[x*2][y*2] = k;
 			}
+		
+
+					
 			solve();
 			
-			bw.write("#" + test_case + " ");
-			bw.write(result);
-			bw.newLine();
+			System.out.printf("#%d %d\n", test_case, result);
 		}//end of testcase
 		
-		bw.flush();
-		bw.close();
+
 	}
 	
 	
 	
-
-	//모든 원자 이동 시킨 후 충돌시키기
 	private static void solve() {
 		
 		while(true) {
-			if(lists.size() <= 0) break;
+			if(queue.size() <= 0) break;
 			
-			List<Pair> temps = new LinkedList<Pair>();
-			for(Pair now : lists) {
+			Queue<Point> temp = new LinkedList<Point>();
+			for(Point now : queue) {
 				
 				int nowX = now.x;
 				int nowY = now.y;
 				int nowDir = now.dir;
 				int nowEnergy = now.energy;
 				
+				maps[nowX][nowY] = 0;//지나가니까 0으로 
+				
 				int nextX = nowX + delta[nowDir][0];
 				int nextY = nowY + delta[nowDir][1];
-				
-				if(nextX < 2000 || nextY >= 4000 || nextY < 2000 || nextY >= 4000) continue;
-				
-				temps.add(new Pair(nextX, nextY, nowDir, nowEnergy));
-			}
 			
-			lists.clear();
-			lists = bomb(temps);
+				if(nextX < 0 || nextX > 4000 || nextY < 0 || nextY > 4000) 
+					continue;
+				else 
+					temp.add(new Point(nextX, nextY, nowDir, nowEnergy));
+			}//for문 종료-> 1초 지남
+		
+			queue.clear();
+			bomb(temp);
 		}
 		
 	}
 	
 	//원자를 충돌시킴
-	private static List<Pair> bomb(List<Pair> temps) {
-		for (int i = 0; i < temps.size()-1; i++) {
-			
-			int sum = temps.get(i).energy;
-			int start = i;
-			//같은 좌표  찾기
-			while(i+1 < temps.size() - 1 && temps.get(i).x == temps.get(i+1).x && temps.get(i).y == temps.get(i+1).y) {
-				sum += temps.get(i+1).energy;
-				result = sum;
-				i++;
-			}                                       
-			
-			temps.get(start).energy = sum;
-			
-			for (int j = start; j <= i; j++) {
-				temps.remove(j);
-				i--;
-				j--;
-			}
-
-		}
+	private static void bomb(Queue<Point> temp) {
 		
-		return temps;
+		while(!temp.isEmpty()) {
+			Point next = temp.poll();
+			int nextX = next.x;
+			int nextY = next.y;
+			int nextDir = next.dir;
+			int nextEnergy = next.energy;
+						
+			
+			if(maps[nextX][nextY] == 0) {//충돌 x
+				maps[nextX][nextY] = nextEnergy;
+				queue.add(new Point(nextX, nextY, nextDir, nextEnergy));
+			}else {//충돌 
+				maps[nextX][nextY] = 0;
+				result += nextEnergy;
+			}
+		}
+
 	}
 	
 }
+
+
+
+
